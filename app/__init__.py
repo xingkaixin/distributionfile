4b545b262aa71611a10443da4b04277957c0a72b
XingKaiXin <xingkaixin@gmail.com>
2015-11-13 19:12:38 +0800

    update file dog

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
 import time
 import logging
 from watchdog.observers import Observer
@@ -15,18 +16,27 @@ class MyFileMonitor(FileSystemEventHandler):
                 filepath=event.src_path))
 
     def on_modified(self, event):
-        super(MyFileMonitor, self).on_created(event)
+        super(MyFileMonitor, self).on_modified(event)
         if not event.is_directory:
             logging.info("modified name:[{filepath}]".format(
                 filepath=event.src_path))
-            CheckFileisOK(event.src_path)
+            if CheckFileisOK(event.src_path):
+                UploadFile(event.src_path)
+
+    def on_moved(self, event):
+        super(MyFileMonitor, self).on_moved(event)
+        if not event.is_directory:
+            logging.info("moved name:[{filepath}] to name:[{newfilepath}] ".format(
+                filepath=event.src_path, newfilepath=event.dest_path))
+            if CheckFileisOK(event.dest_path):
+                UploadFile(event.dest_path)
 
 
 def createDogService():
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = 'D:\work\workspace\distributionfile\data'
+    path = 'D:\\work\\workspace\\distributionfile\\data'
     logging.info('DogService Start...')
     event_handler = MyFileMonitor()
     observer = Observer()
@@ -41,6 +51,25 @@ def createDogService():
     observer.join()
 
 
-def CheckFileisOK(filename):
+def CheckFileisOK(filepath, filesize=0):
     from os.path import getsize
-    logging.debug('{filename} size is {size}'.format(filename=filename,size=getsize(filename)))
+    newsize = getsize(filepath)
+    logging.debug('{filepath} size is {size}'.format(
+        filepath=filepath, size=newsize))
+    if newsize == filesize:
+        logging.debug('Checkfile is Ok')
+        return True
+    logging.debug('Checkfile is not Ok,pending 10 seconds')
+    time.sleep(2)
+    if CheckFileisOK(filepath, newsize):
+        return True
+
+
+def UploadFile(filepath):
+    import shutil
+    from os.path import basename
+    logging.info('Copy file {filepath} Begin'.format(filepath=filepath))
+    dest_path = 'D:\\work\\workspace\\distributionfile\\fakehub\\{filename}'
+    filename = basename(filepath)
+    shutil.copyfile(filepath, dest_path.format(filename=filename))
+    logging.info('Copy file {filepath} Success'.format(filepath=filepath))
