2a5570375d4c38d4557c41149ff2bc1d1210c24c
XingKaiXin <xingkaixin@gmail.com>
2015-11-27 10:42:23 +0800

    update bin

--- bin/beat.sh
+++ bin/beat.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+pid=`ps -ef| grep "celery beat" | grep -v grep | head -n 1 | awk '{print $2}'`
+
+
+while(($pid>0))
+do
+    kill -9 $pid
+    pid=`ps -ef| grep "celery beat" | grep -v grep | head -n 1 | awk '{print $2}'`
+done
+
+
 source ~/.bash_profile
 source ~/venv/bin/activate
 cd ../app
--- bin/dog.sh
+++ bin/dog.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+pid=`ps -ef| grep "python dogserver.py" | grep -v grep | head -n 1 | awk '{print $2}'`
+
+
+while(($pid>0))
+do
+    kill -9 $pid
+    pid=`ps -ef| grep "python dogserver.py" | grep -v grep | head -n 1 | awk '{print $2}'`
+done
+
+
+
 source ~/.bash_profile
 source ~/venv/bin/activate
 cd ../app
